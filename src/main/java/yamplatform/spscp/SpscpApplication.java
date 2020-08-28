e7f87c6c9558ada23bdb6ef32bb60e0bd481d646
sea <1826859775@qq.com>
2020-08-28 10:37:07 +0800

    first commit

---
+++ src/main/java/yamplatform/spscp/SpscpApplication.java
@@ -0,0 +1,13 @@
+package yamplatform.spscp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpscpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpscpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yamplatform/spscp/SpscpApplicationTests.java
@@ -0,0 +1,13 @@
+package yamplatform.spscp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpscpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
