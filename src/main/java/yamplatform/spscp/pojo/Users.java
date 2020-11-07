957f38ea6f404328ae1896e0aaff89acd6f996c4
sea <1826859775@qq.com>
2020-11-07 15:49:25 +0800

    修改用户默认图片

--- src/main/java/yamplatform/spscp/pojo/Users.java
+++ src/main/java/yamplatform/spscp/pojo/Users.java
@@ -10,7 +10,7 @@ public class Users {
     private String password;
     private String nickname; // 昵称
     private String sex = "男";
-    private String picture = "http://localhost:8080/img/Avatar/default.jpg"; // 头像
+    private String picture = "http://cdn.potatopic.top/face_default.jpg"; // 头像
     private String email = "";
     private String comefrom = ""; // 来自
     private String introduction = ""; // 个人简介
